bebd990b781478ff124ec08e59529accdcb85124
leConquistador <danmelnic02@gmail.com>
2025-10-22 10:42:33 +0200

    J'ai rajouté des paramètres pour le maillage

--- cylinder/Cylinder.py
+++ cylinder/Cylinder.py
@@ -17,11 +17,14 @@ class Cylinder(object):
     
     """
 
-    def __init__(self, radius, height, square_length,mesh_format,filename,output_dir):
+    def __init__(self, radius, height, square_length,n_seg_length, n_seg_axial, n_seg_circum,mesh_format,filename,output_dir):
         """ Initialize the attributes """
         self.radius = radius
         self.height = height
         self.square_length = square_length
+        self.n_length = n_seg_length
+        self.n_axial = n_seg_axial
+        self.n_circum = n_seg_circum
         self.mesh_format = mesh_format
         self.filename = filename
         self.output_dir = output_dir
@@ -143,6 +146,53 @@ class Cylinder(object):
         Group_3.setName("Inlet")
         Group_3.result().setName("Inlet")
 
+        ### Create Group
+        Group_4_objects = [model.selection("EDGE", "Split_1_1_2/Generated_Edge&Plane_1/Plane_1&Cylinder_1_1/Face_1"),
+                        model.selection("EDGE", "[Split_1_1_5/Modified_Face&Cylinder_1_1/Face_1][Split_1_1_5/Modified_Face&Plane_2/Plane_2]"),
+                        model.selection("EDGE", "Split_1_1_5/Generated_Edge&Plane_1/Plane_1&Cylinder_1_1/Face_1"),
+                        model.selection("EDGE", "Split_1_1_2/Generated_Edge&Plane_2/Plane_2&Cylinder_1_1/Face_1"),
+                        model.selection("EDGE", "[Extrusion_1_1/Generated_Face&Sketch_1/SketchLine_3][Extrusion_1_1/Generated_Face&Sketch_1/SketchLine_2]"),
+                        model.selection("EDGE", "[Split_1_1_5/Modified_Face&Plane_2/Plane_2][Extrusion_1_1/Generated_Face&Sketch_1/SketchLine_4]"),
+                        model.selection("EDGE", "[Split_1_1_5/Modified_Face&Plane_1/Plane_1][Extrusion_1_1/Generated_Face&Sketch_1/SketchLine_4]"),
+                        model.selection("EDGE", "[Extrusion_1_1/Generated_Face&Sketch_1/SketchLine_2][Extrusion_1_1/Generated_Face&Sketch_1/SketchLine_1]")]
+        Group_4 = model.addGroup(Part_1_doc, "Edges", Group_4_objects)
+        Group_4.setName("n_length")
+        Group_4.result().setName("n_length")
+
+        ### Create Group
+        Group_5_objects = [model.selection("EDGE", "[Split_1_1_4/Modified_Face&Cylinder_1_1/Face_3][Extrusion_1_1/Generated_Face&Sketch_1/SketchLine_3]"),
+                        model.selection("EDGE", "[Extrusion_1_1/Generated_Face&Sketch_1/SketchLine_2][Extrusion_1_1/From_Face]"),
+                        model.selection("EDGE", "[Extrusion_1_1/Generated_Face&Sketch_1/SketchLine_1][Extrusion_1_1/From_Face]"),
+                        model.selection("EDGE", "[Extrusion_1_1/Generated_Face&Sketch_1/SketchLine_4][Extrusion_1_1/From_Face]"),
+                        model.selection("EDGE", "[Split_1_1_5/Modified_Face&Cylinder_1_1/Face_1][Split_1_1_5/Modified_Face&Cylinder_1_1/Face_3]"),
+                        model.selection("EDGE", "[Split_1_1_4/Modified_Face&Cylinder_1_1/Face_1][Split_1_1_4/Modified_Face&Cylinder_1_1/Face_3]"),
+                        model.selection("EDGE", "[Split_1_1_2/Modified_Face&Cylinder_1_1/Face_1][Split_1_1_2/Modified_Face&Cylinder_1_1/Face_3]"),
+                        model.selection("EDGE", "[Split_1_1_3/Modified_Face&Cylinder_1_1/Face_1][Split_1_1_3/Modified_Face&Cylinder_1_1/Face_3]"),
+                        model.selection("EDGE", "[Split_1_1_4/Modified_Face&Cylinder_1_1/Face_1][Split_1_1_4/Modified_Face&Cylinder_1_1/Face_2]"),
+                        model.selection("EDGE", "[Split_1_1_5/Modified_Face&Cylinder_1_1/Face_1][Split_1_1_5/Modified_Face&Cylinder_1_1/Face_2]"),
+                        model.selection("EDGE", "[Split_1_1_3/Modified_Face&Cylinder_1_1/Face_1][Split_1_1_3/Modified_Face&Cylinder_1_1/Face_2]"),
+                        model.selection("EDGE", "[Split_1_1_2/Modified_Face&Cylinder_1_1/Face_1][Split_1_1_2/Modified_Face&Cylinder_1_1/Face_2]"),
+                        model.selection("EDGE", "[Extrusion_1_1/Generated_Face&Sketch_1/SketchLine_3][Extrusion_1_1/To_Face]"),
+                        model.selection("EDGE", "[Extrusion_1_1/Generated_Face&Sketch_1/SketchLine_4][Extrusion_1_1/To_Face]"),
+                        model.selection("EDGE", "[Extrusion_1_1/Generated_Face&Sketch_1/SketchLine_2][Extrusion_1_1/To_Face]"),
+                        model.selection("EDGE", "[Extrusion_1_1/Generated_Face&Sketch_1/SketchLine_1][Extrusion_1_1/To_Face]")]
+        Group_5 = model.addGroup(Part_1_doc, "Edges", Group_5_objects)
+        Group_5.setName("n_circum")
+        Group_5.result().setName("n_circum")
+
+        ### Create Group
+        Group_6_objects = [model.selection("EDGE", "Split_1_1_4/Generated_Edge&Plane_2/Plane_2&Cylinder_1_1/Face_3"),
+                        model.selection("EDGE", "Split_1_1_2/Generated_Edge&Plane_1/Plane_1&Cylinder_1_1/Face_3"),
+                        model.selection("EDGE", "Split_1_1_2/Generated_Edge&Plane_2/Plane_2&Cylinder_1_1/Face_3"),
+                        model.selection("EDGE", "Split_1_1_5/Generated_Edge&Plane_1/Plane_1&Cylinder_1_1/Face_3"),
+                        model.selection("EDGE", "Split_1_1_2/Generated_Edge&Plane_2/Plane_2&Cylinder_1_1/Face_2"),
+                        model.selection("EDGE", "Split_1_1_5/Generated_Edge&Plane_1/Plane_1&Cylinder_1_1/Face_2"),
+                        model.selection("EDGE", "Split_1_1_2/Generated_Edge&Plane_1/Plane_1&Cylinder_1_1/Face_2"),
+                        model.selection("EDGE", "Split_1_1_5/Generated_Edge&Plane_2/Plane_2&Cylinder_1_1/Face_2")]
+        Group_6 = model.addGroup(Part_1_doc, "Edges", Group_6_objects)
+        Group_6.setName("n_axial")
+        Group_6.result().setName("n_axial")
+
         model.end()
 
         ###
@@ -151,7 +201,7 @@ class Cylinder(object):
 
         model.publishToShaperStudy()
 
-        Split_1_1, Walls, Outlet, Inlet, = SHAPERSTUDY.shape(model.featureStringId(Split_1))
+        Split_1_1, Walls, Outlet, Inlet, n_length, n_circum, n_axial, = SHAPERSTUDY.shape(model.featureStringId(Split_1))
         ###
         ### GEOM component
         ###
@@ -175,28 +225,53 @@ class Cylinder(object):
         #smesh.SetEnablePublish( False ) # Set to False to avoid publish in study if not needed or in some particular situations:
                                         # multiples meshes built in parallel, complex and numerous mesh edition (performance)
 
+        try:
+            pass
+        except:
+            print('ExportSTL() failed. Invalid file name?')
         Mesh_1 = smesh.Mesh(Split_1_1,'Mesh_1')
         Regular_1D = Mesh_1.Segment()
-        Number_of_Segments_1 = Regular_1D.NumberOfSegments(15)
+        Number_of_Segments_1 = Regular_1D.NumberOfSegments(15) ##CHECK
         Quadrangle_2D = Mesh_1.Quadrangle(algo=smeshBuilder.QUADRANGLE)
         Hexa_3D = Mesh_1.Hexahedron(algo=smeshBuilder.Hexa)
         Walls_1 = Mesh_1.GroupOnGeom(Walls,'Walls',SMESH.FACE)
         Outlet_1 = Mesh_1.GroupOnGeom(Outlet,'Outlet',SMESH.FACE)
         Inlet_1 = Mesh_1.GroupOnGeom(Inlet,'Inlet',SMESH.FACE)
+        n_length_1 = Mesh_1.GroupOnGeom(n_length,'n_length',SMESH.EDGE)
+        n_circum_1 = Mesh_1.GroupOnGeom(n_circum,'n_circum',SMESH.EDGE)
+        n_axial_1 = Mesh_1.GroupOnGeom(n_axial,'n_axial',SMESH.EDGE)
+        Regular_1D_1 = Mesh_1.Segment(geom=n_length)
+        n_length_2 = Regular_1D_1.NumberOfSegments(self.n_length)
+        Regular_1D_2 = Mesh_1.Segment(geom=n_circum)
+        n_circum_2 = Regular_1D_2.NumberOfSegments(self.n_circum)
+        Regular_1D_3 = Mesh_1.Segment(geom=n_axial)
+        n_axial_2 = Regular_1D_3.NumberOfSegments(self.n_axial)
         isDone = Mesh_1.Compute()
         Mesh_1.CheckCompute()
-        [ Walls_1, Outlet_1, Inlet_1 ] = Mesh_1.GetGroups()
+        [ Walls_1, Outlet_1, Inlet_1, n_length_1, n_circum_1, n_axial_1 ] = Mesh_1.GetGroups()
+        n_length_3 = Regular_1D_1.GetSubMesh()
+        n_circum_3 = Regular_1D_2.GetSubMesh()
+        Sub_mesh_1 = Regular_1D_3.GetSubMesh()
 
 
         ## Set names of Mesh objects
-        smesh.SetName(Walls_1, 'Walls')
-        smesh.SetName(Mesh_1.GetMesh(), 'Mesh_1')
-        smesh.SetName(Inlet_1, 'Inlet')
-        smesh.SetName(Quadrangle_2D.GetAlgorithm(), 'Quadrangle_2D')
+        smesh.SetName(n_axial_1, 'n_axial')
+        smesh.SetName(Sub_mesh_1, 'Sub-mesh_1')
         smesh.SetName(Number_of_Segments_1, 'Number of Segments_1')
-        smesh.SetName(Regular_1D.GetAlgorithm(), 'Regular_1D')
         smesh.SetName(Hexa_3D.GetAlgorithm(), 'Hexa_3D')
+        smesh.SetName(Mesh_1.GetMesh(), 'Mesh_1')
+        smesh.SetName(Quadrangle_2D.GetAlgorithm(), 'Quadrangle_2D')
+        smesh.SetName(n_circum_1, 'n_circum')
+        smesh.SetName(Walls_1, 'Walls')
+        smesh.SetName(n_circum_2, 'n_circum')
+        smesh.SetName(n_length_2, 'n_length')
+        smesh.SetName(Inlet_1, 'Inlet')
+        smesh.SetName(n_length_3, 'n_length')
+        smesh.SetName(n_axial_2, 'n_axial')
         smesh.SetName(Outlet_1, 'Outlet')
+        smesh.SetName(Regular_1D.GetAlgorithm(), 'Regular_1D')
+        smesh.SetName(n_circum_3, 'n_circum')
+        smesh.SetName(n_length_1, 'n_length')
 
         # === EXPORT MESH TO FILE ===
 
