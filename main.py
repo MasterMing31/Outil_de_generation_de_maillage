d2db88bb7359dcec48a770471a1fb719b6d98f06
leConquistador <danmelnic02@gmail.com>
2025-10-23 18:06:13 +0200

    J'ai adapté le main à la nouvelle classe

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Outil de génération de maillage sur Salomé
 """
 from Cylinder import Cylinder
 
-cyl  = Cylinder(radius=10,height=20,square_length=5,n_seg_length=10,n_seg_axial=20,n_seg_circum=15,mesh_format="stl",filename="Mesh",
+cyl  = Cylinder(radius=10,height=10,csquare_length=5,csquare_radius=3,n_seg=20,z_steps=10,mesh_format="stl",filename="Mesh",
                 output_dir=r"D:\SALOME")
 cyl.build_mesh()
 
